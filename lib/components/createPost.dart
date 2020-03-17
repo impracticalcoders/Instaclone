977d5c9c29d37891b7eddc504be7c6e6e5d2d87b
psk907 <p.surajkumar907@gmail.com>
2020-03-17 14:27:15 +0530

    add post ui refinement

--- lib/components/createPost.dart
+++ lib/components/createPost.dart
@@ -8,10 +8,7 @@ import 'package:random_string/random_string.dart';
 
 import 'mainfeed.dart';
 
-
 class CreatePost extends StatefulWidget {
-
-
   @override
   _CreatePostState createState() => _CreatePostState();
 }
@@ -22,12 +19,15 @@ class _CreatePostState extends State<CreatePost> {
   bool isImageUploading = false;
   bool isPosting = false;
   TextEditingController captionController = TextEditingController();
-  
+
   var _scaffoldKey = new GlobalKey<ScaffoldState>();
 
-
   Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.gallery,imageQuality: 50,maxHeight: 500, maxWidth: 500);
+    var image = await ImagePicker.pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 50,
+        maxHeight: 500,
+        maxWidth: 500);
 
     setState(() {
       this._image = image;
@@ -35,100 +35,121 @@ class _CreatePostState extends State<CreatePost> {
     _uploadImage();
   }
 
-  _createPostRequest() async{
-  
-  if(!this.isImageUploading ){
-    
-    setState(() {
-      this.isPosting = true;
-    });
+  _createPostRequest() async {
+    if (!this.isImageUploading) {
+      setState(() {
+        this.isPosting = true;
+      });
 
-    final response = await http.post("https://insta-clone-backend.now.sh/feed",
-      headers:{
-        "Content-type": "application/json"
-      },
-      body:'{"caption":"${captionController.text}","post_pic":"${this.imageUrl}","username":"dummy","profile_name":"dummy"}'
-    );
+      final response = await http.post(
+          "https://insta-clone-backend.now.sh/feed",
+          headers: {"Content-type": "application/json"},
+          body:
+              '{"caption":"${captionController.text}","post_pic":"${this.imageUrl}","username":"dummy","profile_name":"dummy"}');
 
-    print("Status code ${response.statusCode}");
+      print("Status code ${response.statusCode}");
 
-    if(response.statusCode==200){
+      if (response.statusCode == 200) {
         var snackbar = new SnackBar(content: new Text("Posted!"));
         _scaffoldKey.currentState.showSnackBar(snackbar);
-    }
-    else {
-      var snackbar = new SnackBar(content: new Text("Please try again later!"));
+      } else {
+        var snackbar =
+            new SnackBar(content: new Text("Please try again later!"));
         _scaffoldKey.currentState.showSnackBar(snackbar);
+      }
+      setState(() {
+        this.isPosting = false;
+      });
+    } else {
+      var snackbar = new SnackBar(
+          content: new Text("Please wait while the image is uploading!"));
+      _scaffoldKey.currentState.showSnackBar(snackbar);
     }
+  }
+
+  _uploadImage() async {
     setState(() {
-      this.isPosting= false;
+      this.isImageUploading = true;
     });
-    
-  }
-  else{
-    var snackbar = new SnackBar(content: new Text("Please wait while the image is uploading!"));
-        _scaffoldKey.currentState.showSnackBar(snackbar);
-  }
-  
+    String randomString = randomAlphaNumeric(10);
+
+    StorageReference ref =
+        FirebaseStorage.instance.ref().child('images/' + randomString + '.png');
+    StorageUploadTask task = ref.putFile(this._image);
+    StorageTaskSnapshot downloadUrl = (await task.onComplete);
+    String url = (await downloadUrl.ref.getDownloadURL());
+
+    setState(() {
+      this.imageUrl = url;
+      this.isImageUploading = false;
+    });
+
+    print(url);
   }
 
-  _uploadImage() async{
-      setState(() {
-        this.isImageUploading = true;
-        
-      });
-      String randomString = randomAlphaNumeric(10);
-    
-      StorageReference ref = FirebaseStorage.instance.ref().child('images/'+randomString+'.png');
-      StorageUploadTask task =  ref.putFile(this._image);
-      StorageTaskSnapshot downloadUrl = (await task.onComplete);
-      String url = (await downloadUrl.ref.getDownloadURL());
-      
-      setState(() {
-        this.imageUrl = url;
-        this.isImageUploading = false;
-      });
-
-      print(url);
-    }
-  
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      key:_scaffoldKey,
-      
-      appBar: AppBar(title: Text("create new post"),),
-      body:  Container(
-          padding: EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Form(
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(labelText: "Caption"),
-                    autocorrect: false,
-                    controller:captionController ,
+        key: _scaffoldKey,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 10,
+              ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  "New Post",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                child: SingleChildScrollView(
+                  child: Form(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          child: _image == null
+                              ? FlatButton(
+                                  onPressed: getImage,
+                                  child: SizedBox(
+                                    height: 300,
+                                    width: 300,
+                                    child: Icon(Icons.add_a_photo),
+                                  ),
+                                )
+                              : Image.file(
+                                  _image,
+                                  height: 300,
+                                ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Caption"),
+                          autocorrect: false,
+                          controller: captionController,
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        RaisedButton(
+                          child: Text("Post"),
+                          onPressed:
+                              this.isPosting ? () {} : _createPostRequest,
+                        ),
+                        Container(
+                          child: this.isPosting
+                              ? CircularProgressIndicator()
+                              : Text(""),
+                        )
+                      ],
+                    ),
                   ),
-                  RaisedButton(
-                    onPressed: getImage,
-                    child: Icon(Icons.add_a_photo),
-                  ),
-                  Container(
-                    child:_image == null
-                      ? Text('No image selected.')
-                      : Image.file(_image,height: 300,),),
-                  RaisedButton(
-                    child:Text("Post"),
-                    onPressed:this.isPosting?(){}:_createPostRequest,
-                    
-                     ),
-                  Container(
-                    child:this.isPosting?CircularProgressIndicator():Text(""),)                  
-                ],),
-            ),
-          ),)
-      );
-
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
