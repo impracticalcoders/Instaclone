b9c2044575931b4c89d1a146614ecfb8b2c9b54b
Soundarya03 <soundarya1103@gmail.com>
2020-03-18 19:34:00 +0530

    sign-up

--- lib/components/Signup.dart
+++ lib/components/Signup.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
-@override
-_SignupPageState createState() => new _SignupPageState();
+  @override
+  _SignupPageState createState() => new _SignupPageState();
 }
 
 class _SignupPageState extends State<SignupPage> {
@@ -12,11 +12,94 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-          centerTitle: true,
-          title:appBarTitle,
-          
+        centerTitle: true,
+        title: appBarTitle,
       ),
-      body: Text("hi"),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 80, 30, 0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(
+                'InstaClone',
+                style: TextStyle(
+                  fontFamily: 'Billabong',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+              Divider(
+                height: 35,
+                color: Colors.red[600],
+              ),
+              Text(
+                'Sign up to check out your feed!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+              Container(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Name',
+                          hintStyle: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Username',
+                          hintStyle: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Bio',
+                          hintStyle: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              color: Colors.red[600],
+                              child: FlatButton(
+                                onPressed: null,
+                                child: Text(
+                                  'Done',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ]),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
